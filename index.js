39f00fac83f5029b2585175e936abf9281df12ec
John Thummel <enikath@gmail.com>
2021-07-31 22:19:49 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+var myNewP = document.createElement("p");
+myNewP.innerHTML = "This is a paragraph.";
